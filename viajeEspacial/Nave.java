6f717281d60bf425446c6d76c296449544f3fd12
SeronLuciano <seronluciano514@gmail.com>
2025-09-04 07:15:49 +0000

    algorito nave espacial TERMINADO

--- viajeEspacial/Nave.java
+++ viajeEspacial/Nave.java
@@ -6,32 +6,60 @@ public class Nave {
     private int combustible;
     private String destino;
     private int velocidadMax;
-    private int restanteCombustible;
-
-    // costructor 
-    public Nave (String nombre, int velocidad,int combustible, String destino){
+    
+// -----CONSTRUCTOR---
+    public Nave (String nombre,int combustible, String destino,  int velocidadMax){
         this.nombre = nombre;
-        this.velocidad = velocidad;
+        this.velocidad = 0;
         this.combustible = combustible;
         this.destino = destino;
-       
+        this.velocidadMax = velocidadMax;
     }
-    // metodos 
-    // subir y bajar velocidad
-    
-    public void acelerar (){
-      
-                velocidad = velocidad + 20;
-            }
- 
+   
+// ------METODOS--------
 
+// ----GET----
+public int getVelocidad() {
+    return velocidad;
+}
+public String getNombre() {
+    return nombre;
+}
+public int getCombustible() {
+    return combustible;
+}
+
+public String getDestino() {
+    return destino;
+}
+
+// ---METODO ACELERAR---
+    public void acelerar (int incremento){
+        velocidad = velocidad + incremento;
+
+        if (velocidad > velocidadMax){
+            velocidad = velocidadMax;
         }
+
     }
-    public void bajarVelocidad (){
-            velocidad = velocidad- 20;
+    
+// ------METODO LLEGA A DESTINO----
+    public boolean llegaDestino(){
+        if (combustible < 100){
+            return false;
+         } else { 
+                return true;
+            }
         }
-    }
-    public int getVelocidad(){
-        return velocidad;
-    }
+
+//METODO INFORME
+public void Informe (){
+    System.out.println("------INFORME DE LA MISION------  ");
+    System.out.println("DESTINO: "+ destino);
+    System.out.println("VELOCIDAD ACTUAL: "+ velocidad +" / "+ "VELOCIDAD MAXIMA: "+velocidadMax);
+    System.out.println("COMBUSTIBLE RESTANTE: "+ combustible);
+    System.out.println(nombre+" puede llegar a "+ destino + (llegaDestino()?  " SI" : " NO"  ));
+}
+
+
 }
--- viajeEspacial/PruebaNave.java
+++ viajeEspacial/PruebaNave.java
@@ -3,12 +3,31 @@
 public class PruebaNave {
     public static void main(String[] args) {
         
-        Nave nave1 = new Nave ("Seron",80,88,"Luna");
-        nave1.subirVelocidad();
-        System.out.println("velocidad actual"+nave1.getVelocidad());
+        Nave nave1 = new Nave ("Eli",20,"Dios",50000);
+        nave1.acelerar(10000);
+        System.out.println ("Velocidad actual: "+ nave1.getVelocidad() +" km/h");
+        System.out.println();
+        System.out.println ("------ACELERAR------");
+        System.out.println ("ACELERANDO 1°: "+ nave1.getVelocidad()+" km/h");
+        nave1.acelerar(20000);
+        System.out.println ("ACELERANDO 2°: "+ nave1.getVelocidad()+" km/h");
+        nave1.acelerar(4000);
+        System.out.println ("ACELERANDO 3°: "+ nave1.getVelocidad()+" km/h");
+        
 
+        
+   // VERIFICAR SI LLEGA A DESTINO
+    System.out.println();
+    System.out.println (nave1.getNombre()+" puede llegar a "+ nave1.getDestino()+": "+nave1.llegaDestino());
 
+   
+   // INFORME 
+   System.out.println();
+   nave1.Informe();
 
-
+   
+   
+   
+   
     }
 }
