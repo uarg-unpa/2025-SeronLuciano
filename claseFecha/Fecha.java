16cddbcd05cb91dac183d3c299c325f0980901cb
SeronLuciano <seronluciano514@gmail.com>
2025-09-09 20:58:14 +0000

    clase 9 / 9 clase persona y clase fecha (a medias)

---
+++ claseFecha/Fecha.java
@@ -0,0 +1,37 @@
+public class Fecha {
+
+// ATRIBUTOS
+private int dia;
+private int mes;
+private int año;
+
+// VALIDAR fecha
+//constructor 
+public fecha (int dia, int mes, int año){
+ 
+    
+
+public validarFecha (int dia, int mes, int año){
+    if (dia )
+
+
+}
+
+
+
+
+
+
+
+//metodo get 
+public int getDia (){
+    return dia;
+}
+public int getMes () {
+
+}
+public int getAño (){
+
+}
+públic int setDia (){
+}
