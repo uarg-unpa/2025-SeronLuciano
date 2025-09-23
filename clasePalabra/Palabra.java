2a2b276d6e924c28bfb5fffaa7659af765f69878
SeronLuciano <seronluciano514@gmail.com>
2025-09-23 20:41:03 +0000

    clase23/9 practico 3

---
+++ clasePalabra/Palabra.java
@@ -0,0 +1,33 @@
+package clasePalabra;
+
+public class Palabra {
+
+    // atribut0
+private String palabra = 'palabra';
+    private int longitud = palabtra.length();
+
+    // constructor
+    public Palabra(String palabra) {
+        this.palabra = palabra;
+        this.longitud = palabra.length();
+
+    }
+
+    // metodos
+    public String getPalabra (){
+    return palabra;
+
+}
+
+    public int getLongitud() {
+        return longitud;
+
+    }
+
+    
+    toString(){
+        return "palabra"+palabra + "longitud"+longitud;
+
+    }
+}
+}
---
+++ clasePalabra/mainPalabra.java
@@ -0,0 +1,17 @@
+package clasePalabra;
+
+public class mainPalabra {
+
+    public static 
+    //recorrer palabra
+public void recorrerPalabra(){
+    for (int i = 0; i < palabra.length(); i++) {
+        System.out.println(palabra.charAt(i));
+
+    }
+    //visualizar la palabra al reves
+public void visualizarAlReves (){
+    for (int i = palabra.length() - 1; i >=0; i--){
+        System.out.println(palabra.charAt(i));
+
+}
