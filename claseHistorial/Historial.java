b7559d944def3e8c1dcc2fd587801c835ffce1d8
SeronLuciano <seronluciano514@gmail.com>
2025-10-07 20:43:02 +0000

    punto 4 tp4//  clase 7/10

---
+++ claseHistorial/Historial.java
@@ -0,0 +1,26 @@
+package claseHistorial;
+
+    public class Historial {
+
+    private Url[] paginas;
+    private int cima;
+    private final int MAX = 5;
+
+    public Historial() {
+        paginas = new Url[MAX];
+        cima = -1;
+    }
+
+    public boolean estaVacia() { 
+        if (cima == -1)
+            return true;
+        else
+            return false;
+     }
+    public boolean estaLlena() { return cima == MAX - 1; }
+    public void meter(Url pagina) {
+        cima++;
+        paginas[cima] = pagina;
+    }
+    
+}
---
+++ punto4/Contar.java
@@ -0,0 +1,25 @@
+public class Contar {
+
+
+// atribtos 
+private int Max = 5;
+private int cola [];
+private int frente,ultimo;
+private int cont;
+
+//constructor 
+public Contar(){
+    cola = new int [Max];
+    frente = 0 ;
+    ultimo = 0;
+    }
+
+// metoso sig
+private int siguiente (int subint){
+    if (subint == Max -1){
+        rewtui
+    }
+}
+
+}
+}
